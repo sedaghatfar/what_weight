a91b12d1ad86afd6944097d4c836477a6f173ac3
Matthew Sedaghatfar <sedaghatfar@gmail.com>
2025-04-10 08:19:10 -0400

    Update streamlit_app.py
    update to use use_container_width

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,20 +10,17 @@ def process_file(uploaded_file):
     speakers = []
     msgs = []
     weights = []
-
     for line in uploaded_file:
         line = line.decode("utf-8")  # Decoding to convert from binary
         date_match = re.search(r'\[(.*?)\]', line)
         speaker_match = re.search(r'\] (.*?):', line)
         msg_match = re.search(r': (.*)', line)
         weight_match = re.search(r': (\d+)', line)
-
         if date_match and speaker_match and msg_match and weight_match:
             dates.append(date_match.group(1))
             speakers.append(speaker_match.group(1))
             msgs.append(msg_match.group(1))
             weights.append(weight_match.group(1))
-
     df = pd.DataFrame({
         'Date': pd.to_datetime(dates),
         'Speaker': speakers,
@@ -42,63 +39,58 @@ def remove_outliers(df, column):
     return df[(df[column] >= lower_bound) & (df[column] <= upper_bound)]
 
 # Streamlit app
-# st.image('weds_export.jpeg', use_column_width=True)
 def main():
     st.title("Weight What[sApp]?")
-
+    
     # Layout for image and text
     col1, col2 = st.columns([1, 2])
     with col1:
-        st.image('/mount/src/what_weight/weds_export.jpeg', use_column_width=True)
+        st.image('/mount/src/what_weight/weds_export.jpeg', use_container_width=True)
     with col2:
         st.write("\n\n\n\n")
         st.write("\n\n\n\n")
         st.write("To easily track my weight I use a Whatsapp groupchat, and created this app to upload an export of the chat")
         st.write("""Then use Regex to parse out date, speaker, and weight to display a chart using plotly.""")
-
+    
     # File uploader
     uploaded_file = st.file_uploader("Choose a file", type="txt")
     if uploaded_file is not None:
         data = process_file(uploaded_file)
         st.write(data)
-
+        
         # Speaker selection
         speaker_list = data['Speaker'].unique()
         selected_speaker = st.selectbox("Select a Speaker", speaker_list)
-
+        
         # Checkbox for outlier removal
         remove_outliers_option = st.checkbox("Remove Outliers")
-
+        
         # Filter data based on selected speaker
         filtered_data = data[data['Speaker'] == selected_speaker]
-
+        
         # Remove outliers if option is selected
         if remove_outliers_option:
             filtered_data = remove_outliers(filtered_data, 'Weight')
-
+        
         # Plot
         fig = px.scatter(filtered_data, x='Date', y='Weight', hover_data=['Msg'], color='Speaker')
         fig.update_layout(xaxis_range=[100, None], showlegend=False)
         st.plotly_chart(fig)
-
+    
     st.title("My Weight in 2023")
-
+    
     # Layout for description
     col1, col2 = st.columns([1, 2])
     with col1:
-        st.image('/mount/src/what_weight/my_weight.jpeg', use_column_width=True)
+        st.image('/mount/src/what_weight/my_weight.jpeg', use_container_width=True)
     with col2:
         st.write("\n\n\n\n")
         st.write("\n\n\n\n")
         st.write("I spent the first part of 2023 going from a Skinny fat 150-155 to losing 10 pounds using IF and a CGM")
         st.write("\n\n\n\n")
         st.write("""After losing the dead weight I bulked back to the 155 and hit PR's in all weightlifting areas""")
-
-    st.image('/mount/src/what_weight/progress_resized.png', use_column_width=True)
-
-    # File uploader    
-
+    
+    st.image('/mount/src/what_weight/progress_resized.png', use_container_width=True)
         
-
 if __name__ == "__main__":
     main()
